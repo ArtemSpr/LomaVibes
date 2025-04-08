3f45f699cb7ab098555375195a6156f4ce0111c9
ArtemSpr <artemspravnikov77@gmail.com>
2025-04-08 19:09:26 +0300

    Add animated block

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setVisible(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="animateContainer">
+      <AnimatePresence>
+        {visible && (
+          <motion.div
+            initial={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, scale: 0.8, y: -50 }}
+            transition={{ duration: 1 }}
+            className="bg-blue-500 text-white p-6 rounded-2xl shadow-lg text-xl"
+          >
+            <span className="animateItem"> Я зникну за 2 секунди ✨</span>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      sd
+    </div>
+  );
 }
-
-export default App
