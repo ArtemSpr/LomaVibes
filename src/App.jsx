34a763b628073985b6bbb1a185901b2514225a93
WellNoteOne <165422516+WellNoteOne@users.noreply.github.com>
2025-04-08 19:02:33 +0300

    1. Deleted original code from App.jsx 2.Added map to projects 3.Added some new styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./App.css";
+
+const INITIAL_CENTER = [24.9411, 60.1727];
+const INITIAL_ZOOM = 11.12;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [center, setCenter] = useState(INITIAL_CENTER);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
+
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoid2VsbG5vdGUiLCJhIjoiY205ODZuODJqMGV4eDJsc2VwZ3hraHAzaSJ9.Ei8toDcIcpyPbVlu6BLkvw";
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: center,
+      zoom: zoom,
+    });
+
+    mapRef.current.on("move", () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter();
+      const mapZoom = mapRef.current.getZoom();
+      const map = mapRef.current;
+      // update state
+      setCenter([mapCenter.lng, mapCenter.lat]);
+      setZoom(mapZoom);
+    });
+
+    new mapboxgl.Marker({ color: "black", rotation: 0 })
+      .setLngLat([24.9645, 60.2104])
+      .addTo(mapRef.current);
+
+    const bus = new mapboxgl.Marker({ color: "green", rotation: 0 })
+      .setLngLat([24.9353, 60.2017])
+      .addTo(mapRef.current);
+
+    mapRef.current.addControl(new mapboxgl.FullscreenControl());
+
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    mapRef.current.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM,
+    });
+  };
+  const handleButtonClicktoBus = () => {
+    mapRef.current.flyTo({
+      center: [24.9353, 60.2017],
+      zoom: 16.12,
+    });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 class="font-effect-anaglyph" className="header">
+        LomaVibes
+      </h1>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <button className="reset-button" onClick={handleButtonClick}>
+        Reset
+      </button>
+      <button className="college-button" onClick={handleButtonClicktoBus}>
+        Business College Helsinki
+      </button>
+
+      <div id="map-container" ref={mapContainerRef} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
