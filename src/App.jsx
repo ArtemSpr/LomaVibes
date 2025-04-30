833e64f097f0672413911ef8d44b23b604122de2
ArtemSpr <artemspravnikov77@gmail.com>
2025-04-30 13:49:08 +0300

    Add main page at mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,86 @@
-import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
 
-export default function App() {
+import Animatelogo from "../public/217490777.png";
+import Menu from "../public/burger-menu.png";
+import ListIcon from "../public/list-icon.svg";
+import JobIcon from "../public/job-icon.svg";
+import SettingsIcon from "../public/settings-icon.svg";
+import CalendarIcon from "../public/calendar-icon.svg";
+
+const App = () => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setVisible(false), 2000);
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 2000);
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="animateContainer">
-      <AnimatePresence>
-        {visible && (
-          <motion.div
-            initial={{ opacity: 1, scale: 1 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0, scale: 0.8, y: -50 }}
-            transition={{ duration: 1 }}
-            className="bg-blue-500 text-white p-6 rounded-2xl shadow-lg text-xl"
-          >
-            <span className="animateItem"> Я зникну за 2 секунди ✨</span>
-          </motion.div>
-        )}
-      </AnimatePresence>
-      sd
+    <div className="main">
+      <div className="animateContainer">
+        <AnimatePresence>
+          {visible && (
+            <motion.div
+              initial={{ opacity: 1, scale: 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, scale: 0.8, y: -50 }}
+              transition={{ duration: 1 }}
+              className="bg-blue-500 text-white p-6 rounded-2xl shadow-lg text-xl"
+            >
+              <span className="animateItem">
+                {" "}
+                <img className="animateLogo" src={Animatelogo}></img>
+              </span>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div className="header">
+        <div className="menu">
+          <img src={Menu}></img>
+        </div>
+        <span className="header-text logo">LomaVibes</span>
+        <span className="header-text">Fi/En</span>
+      </div>
+      <div className="body">
+        <span className="body-item">
+          <div className="event-title">Evnet name </div>
+          <div className="event-content">Eve</div>
+          <div className="event-adres"></div>
+        </span>
+        <span className="body-item"></span>
+        <span className="body-item"></span>
+        <span className="body-item"></span>
+      </div>
+      <div className="nav-bar">
+        <div className="nav-bar-container">
+          <span className="nav-bar-item">
+            <a src="">
+              <img src={JobIcon}></img>
+            </a>
+          </span>
+          <span className="nav-bar-item">
+            <a src="">
+              <img src={CalendarIcon}></img>
+            </a>
+          </span>
+          <span className="nav-bar-item">
+            <a src="">
+              <img src={ListIcon}></img>
+            </a>
+          </span>
+          <span className="nav-bar-item">
+            <a src="">
+              <img src={SettingsIcon}></img>
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
